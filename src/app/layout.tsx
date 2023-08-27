a37cfba584def369fbc8e68fdb1d6c39a4243d2b
JoaoCardoso00 <jvcq13@gmail.com>
2023-08-27 01:37:52 -0300

    add seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Aurify",
   description: "Aurify - Rastreabilidade do Minério de Ouro",
+  openGraph: {
+    type: "website",
+    locale: "pt_BR",
+    url: "https://www.aurify,tech",
+    title: "Aurify",
+    description: "Rastreabilidade do Minério de Ouro Por meio da Blockchain",
+    images: [
+      {
+        url: "/seo.webp",
+        width: 760,
+        height: 460,
+        alt: "Aurify - Rastreabilidade do Minério de Ouro",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
